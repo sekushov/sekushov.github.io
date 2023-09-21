57227ccc65fb1e533d7794b91a6940cb1fddaf22
Tim <sekushov@ya.ru>
2023-09-21 16:37:25 +1200

    modal form

--- rusdrevstroy/js/script.js
+++ rusdrevstroy/js/script.js
@@ -36,8 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //modal
     const modalTriggers = document.querySelectorAll(".modal_trigger"),
-          modal = document.querySelector(".modal"),
-          modalClose = document.querySelector(".modal_close");
+          modal = document.querySelector(".modal");
     function showModal() {
         modal.style.display = "flex";
         document.body.style.overflow = "hidden";
@@ -53,7 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
     modal.addEventListener("click", (e) => {
-        if(e.target === modal || e.target === modalClose) {
+        if(e.target === modal || e.target.classList.contains("modal_close")) {
             hideModal();
         }
     });
@@ -224,6 +223,67 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+
+
+
+
+    // form
+    const forms = document.querySelectorAll("form"),
+          message = {
+            loading: "img/spinner.svg",
+            success: "Данные отправились. Ожидайте звонка в ближайшее время",
+            failure: "Что-то пошло не так"
+          };
+
+    forms.forEach(form => {
+        postData(form);
+    });
+
+    function postData(form) {
+        function showInfoModal(message) {
+            const prevModalContent = document.querySelector(".modal_content");
+            prevModalContent.style.display = "none";
+            showModal();
+            const messageModal = document.createElement("div");
+            messageModal.classList.add("modal_content");
+            messageModal.innerHTML = `
+                <div class="modal_close">&times;</div>
+                <div class="modal_title">${message}</div>
+            `;
+            document.querySelector(".modal").append(messageModal);
+            setTimeout(() => {
+                messageModal.remove();
+                prevModalContent.style.display = "";
+                hideModal();
+            }, 3500);
+        }
+
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            const messageStatus = document.createElement("img");
+            messageStatus.src = message.loading;
+            messageStatus.style.cssText = "display: block; margin: 0 auto";
+            form.append(messageStatus);
+
+            const formData = new FormData(form);
+
+            fetch("server.php", {
+                method: "POST",
+                body: formData
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showInfoModal(message.success);
+                messageStatus.remove()
+            }).catch(() => {
+                showInfoModal(message.failure);
+                messageStatus.remove()
+            }).finally(() => {
+                form.reset();
+            })
+        });
+    }
 
 
 });
